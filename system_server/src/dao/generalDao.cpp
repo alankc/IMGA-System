eae201861088abea7d69dfc20281c1108b4d4bce
Alan Kunz Cechinel <cechinel.a.k@gmail.com>
2020-05-23 21:41:24 -0300

    executeUpdate without transaction

--- system_server/src/dao/generalDao.cpp
+++ system_server/src/dao/generalDao.cpp
@@ -49,3 +49,76 @@ bool GeneralDao::executeQuery(std::string statement, std::unique_ptr<sql::Result
 
     return sucess;
 }
+
+bool GeneralDao::executeUpdate(std::string statement)
+{
+    bool sucess;
+    try
+    {
+        sql::Driver *driver = get_driver_instance();
+
+        std::unique_ptr<sql::Connection> con(driver->connect(host, user, pass));
+        con->setSchema(database);
+
+        std::unique_ptr<sql::PreparedStatement> pstmt;
+        pstmt.reset(con->prepareStatement(statement));
+        sucess = (bool) pstmt->executeUpdate();
+    }
+    catch (sql::SQLException &e)
+    {
+        /*
+      MySQL Connector/C++ throws three different exceptions:
+
+      - sql::MethodNotImplementedException (derived from sql::SQLException)
+      - sql::InvalidArgumentException (derived from sql::SQLException)
+      - sql::SQLException (derived from std::runtime_error)
+    */
+        std::cout << "# ERR: SQLException in " << __FILE__;
+        std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
+        /* what() (derived from std::runtime_error) fetches error message */
+        std::cout << "# ERR: " << e.what();
+        std::cout << " (MySQL error code: " << e.getErrorCode();
+        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
+
+        sucess = false;
+    }
+
+    return sucess;    
+}
+
+bool GeneralDao::executeUpdate(std::vector<std::string> statement)
+{
+    bool sucess;
+    try
+    {
+        /*sql::Driver *driver = get_driver_instance();
+
+        std::unique_ptr<sql::Connection> con(driver->connect(host, user, pass));
+        con->setSchema(database);
+
+        std::unique_ptr<sql::PreparedStatement> pstmt;
+        sql::
+        pstmt.reset(con->prepareStatement(statement));
+        sucess = (bool) pstmt->executeUpdate();*/
+    }
+    catch (sql::SQLException &e)
+    {
+        /*
+      MySQL Connector/C++ throws three different exceptions:
+
+      - sql::MethodNotImplementedException (derived from sql::SQLException)
+      - sql::InvalidArgumentException (derived from sql::SQLException)
+      - sql::SQLException (derived from std::runtime_error)
+    */
+        std::cout << "# ERR: SQLException in " << __FILE__;
+        std::cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << std::endl;
+        /* what() (derived from std::runtime_error) fetches error message */
+        std::cout << "# ERR: " << e.what();
+        std::cout << " (MySQL error code: " << e.getErrorCode();
+        std::cout << ", SQLState: " << e.getSQLState() << " )" << std::endl;
+
+        sucess = false;
+    }
+
+    return sucess;     
+}
--- system_server/src/dao/generalDao.hpp
+++ system_server/src/dao/generalDao.hpp
@@ -19,4 +19,6 @@ public:
     GeneralDao(std::string host, std::string user, std::string pass, std::string database);
     ~GeneralDao();
     bool executeQuery(std::string statement, std::unique_ptr<sql::ResultSet> &res);
+    bool executeUpdate(std::string statement);
+    bool executeUpdate(std::vector<std::string> statement);
 };
