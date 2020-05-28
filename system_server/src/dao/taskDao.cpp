331d94719cb5cc83da33bd59eb7cc04f5f92fc52
Alan Kunz Cechinel <cechinel.a.k@gmail.com>
2020-05-28 18:56:58 -0300

    updates daoTask

--- system_server/src/dao/taskDao.cpp
+++ system_server/src/dao/taskDao.cpp
@@ -1,5 +1,7 @@
 #include "taskDao.hpp"
 
+#include <sstream>
+
 TaskDao::TaskDao(GeneralDao *gDao)
 {
     this->gDao = gDao;
@@ -28,10 +30,76 @@ bool TaskDao::getTaskList(std::vector<Task> &taskList, std::string status)
             tmp.setDescription(res->getString("description"));
             tmp.setRobotInCharge(res->getUInt("id_robot_in_charge"));
             tmp.setSeqNumber(res->getUInt("seq_number"));
-            
+
             taskList.push_back(tmp);
         }
     }
+
+    return tst;
+}
+
+bool TaskDao::updateTask(Task &task)
+{
+    std::ostringstream stmtStream;
+    stmtStream << "UPDATE task SET ";
+    stmtStream << "description = '" << task.getDescription() << "', ";
+    stmtStream << "status = '" << task.getStatus() << "', ";
+    stmtStream << "payload = '" << task.getPayload() << "', ";
+    stmtStream << "deadline = '" << task.getDeadline() << "', ";
+    stmtStream << "id_pick_up_location = '" << task.getPickUpLocation() << "', ";
+    stmtStream << "id_delivery_location = '" << task.getDeliveryLocation() << "', ";
+    stmtStream << "id_robot_in_charge = '" << task.getRobotInCharge() << "', ";
+    stmtStream << "seq_number = '" << task.getSeqNumber() << "' ";
+    stmtStream << "WHERE task.id_task = " << task.getId() << ";";
+
+    bool tst = gDao->executeUpdate(stmtStream.str());
+
+    return tst;
+}
+
+bool TaskDao::updateTask(std::vector<Task> &taskList)
+{
+    std::vector<std::string> statementVector;
+
+    for (auto task : taskList)
+    {
+        std::ostringstream stmtStream;
+        stmtStream << "UPDATE task SET ";
+        stmtStream << "description = '" << task.getDescription() << "', ";
+        stmtStream << "status = '" << task.getStatus() << "', ";
+        stmtStream << "payload = '" << task.getPayload() << "', ";
+        stmtStream << "deadline = '" << task.getDeadline() << "', ";
+        stmtStream << "id_pick_up_location = '" << task.getPickUpLocation() << "', ";
+        stmtStream << "id_delivery_location = '" << task.getDeliveryLocation() << "', ";
+        stmtStream << "id_robot_in_charge = '" << task.getRobotInCharge() << "', ";
+        stmtStream << "seq_number = '" << task.getSeqNumber() << "' ";
+        stmtStream << "WHERE task.id_task = " << task.getId() << ";";
+
+        statementVector.push_back(stmtStream.str());
+    }
+
+    bool tst = gDao->executeUpdate(statementVector);
+
+    return tst;
+}
+
+bool TaskDao::updateTasksScheduled(std::vector<TaskScheduledData> &taskList)
+{
+    std::vector<std::string> statementVector;
+
+    for (auto task : taskList)
+    {
+        std::ostringstream stmtStream;
+        stmtStream << "UPDATE task SET ";
+        stmtStream << "status = '" << task.status << "', ";
+        stmtStream << "id_robot_in_charge = '" << task.robotInCharge << "', ";
+        stmtStream << "seq_number = '" << task.seqNumber << "' ";
+        stmtStream << "WHERE task.id_task = " << task.id << ";";
+
+        statementVector.push_back(stmtStream.str());
+    }
+
+    bool tst = gDao->executeUpdate(statementVector);
 
     return tst;
 }
--- system_server/src/dao/taskDao.hpp
+++ system_server/src/dao/taskDao.hpp
@@ -4,6 +4,15 @@
 #include "generalDao.hpp"
 #include "../model/task.hpp"
 
+typedef struct _taskScheduledData
+{
+	uint32_t id;
+	std::string status;
+	uint32_t robotInCharge;
+	uint32_t seqNumber;
+}TaskScheduledData;
+
+
 class TaskDao
 {
 private:
@@ -13,6 +22,9 @@ public:
     TaskDao(GeneralDao *gDao);
     ~TaskDao();
     bool getTaskList(std::vector<Task> &taskList, std::string status);
+    bool updateTask(Task &task);
+    bool updateTask(std::vector<Task> &taskList);
+    bool updateTasksScheduled(std::vector<TaskScheduledData> &taskList);
 };
 
 #endif
