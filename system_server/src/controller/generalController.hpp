804106a4b13f9f82945ae630809c5bb3a3f312ff
Alan Kunz Cechinel <cechinel.a.k@gmail.com>
2020-08-04 20:33:39 -0300

    navigation stack working

--- system_server/src/controller/generalController.hpp
+++ system_server/src/controller/generalController.hpp
@@ -4,10 +4,21 @@
 #include "robotController.hpp"
 #include "taskController.hpp"
 #include "locationController.hpp"
+#include <ros/ros.h>
+#include <system_server/MsgRequest.h>
 
 class GeneralController
 {
 private:
+    ros::NodeHandle nh;
+    ros::Subscriber subRequest;
+    ros::Subscriber subRobotData;
+    std::map<uint32_t, ros::Publisher> pubRobotRequest;
+    std::map<uint32_t, ros::Publisher> pubRobotTask;
+
+    void callbackRequest(system_server::MsgRequest &msg);
+
+
     GeneralDao gdao;
     RobotController rc;
     TaskController tc;
