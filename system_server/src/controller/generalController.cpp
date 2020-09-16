3751bb587f985d675f80e8748020288712042b27
Alan Kunz Cechinel <cechinel.a.k@gmail.com>
2020-09-16 17:04:49 -0300

    before mount experiment

--- system_server/src/controller/generalController.cpp
+++ system_server/src/controller/generalController.cpp
@@ -38,7 +38,7 @@ void GeneralController::callScheduler()
     Chromosome best = is.getBest();
     best.printResult();*/
 
-    lc.gerenateLocations(10, 3, 10, 50);
+    /*lc.gerenateLocations(10, 3, 10, 50);
     for (uint32_t i = 0; i < lc.getLocationListSize(); i++)
     {
         std::cout << lc.getLocationByIndex(i)->getId() << " "
@@ -58,7 +58,7 @@ void GeneralController::callScheduler()
     }
 
     std::cout << "Robots" << std::endl;
-    rc.generateRobots(3, 1, 10, 0.5, 3, 1.0 / (8 * 60 * 60), 1.0 / (4 * 60 * 60), 3);
+    rc.generateRobots(5, 1, 10, 0.5, 3, 1.0 / (8 * 60 * 60), 1.0 / (4 * 60 * 60), 3);
     auto r = rc.getFreeRobots();
     for (auto it : *r)
     {
@@ -72,7 +72,7 @@ void GeneralController::callScheduler()
     }
 
     std::cout << "Tasks" << std::endl;
-    auto crResult = tc.generateTasks(18, r, d, 3);
+    auto crResult = tc.generateTasks(20, r, d, 3);
     auto t = tc.getTaskList();
     for (auto it : *t)
     {
@@ -81,5 +81,36 @@ void GeneralController::callScheduler()
 
     std::cout << "Chromossome" << std::endl;
 
-    crResult.printResult();
+    crResult.printResult();*/
+    double hitRate = 0.0;
+
+    for (uint32_t i = 0; i < 100; i++)
+    {
+        lc.gerenateLocations(10, 3, 10, 50);
+        auto d = lc.getDistanceMatrix();
+        rc.generateRobots(5, 1, 10, 0.5, 3, 1.0 / (8 * 60 * 60), 1.0 / (4 * 60 * 60), 3);
+        auto r = rc.getFreeRobots();
+        auto crResult = tc.generateTasks(20, r, d, 3);
+
+        GAParameters gaP;
+        gaP.populationSize = 100;
+        gaP.mutationRate = 0.25;
+        gaP.elitismRate = 0.05;
+        gaP.maxIterations = 1000;
+        gaP.noChangeLimit = gaP.maxIterations;
+
+        auto tasks = tc.getTaskList();
+        auto robots = rc.getFreeRobots();
+        auto distance = lc.getDistanceMatrix();
+
+        Island is(gaP, 50, 0.1, tasks, robots, distance);
+        is.solve();
+        Chromosome best = is.getBest();
+        best.printResult();
+
+        if (best.allScheduled())
+            hitRate += 1.0/100.0;
+    }
+
+    std::cout << "HitHate = " << hitRate << std::endl;
 }
