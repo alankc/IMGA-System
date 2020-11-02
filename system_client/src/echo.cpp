73eedb25904dcb3f2809ec2999786dcbb187d9ae
Alan Kunz Cechinel <cechinel.a.k@gmail.com>
2020-11-02 14:39:20 -0300

    reading requests and tasks send by server

--- system_client/src/echo.cpp
+++ system_client/src/echo.cpp
@@ -1,12 +1,25 @@
 #include <ros/ros.h>
-#include <system_server/MsgRequest.h>
+#include <system_client/MsgRequest.h>
 #include <system_server/MsgRobotData.h>
+#include <system_client/MsgTaskList.h>
 #include <iostream>
 
-void callback_RE(const system_server::MsgRequest &msg)
+void callback_RE(const system_client::MsgRequest &msg)
 {
     std::string s = std::to_string(msg.type) + "--" + std::to_string(msg.data);
     std::cout << s << std::endl;
+}
+
+void callback_TL(const system_client::MsgTaskList &msg)
+{
+    for (uint32_t i = 0; i < msg.taskList.size(); i++)
+    {
+        auto& t = msg.taskList[i];
+        std::cout << t.id << " ";
+        std::cout << t.deadline << " ";
+        std::cout << t.pickUp << " ";
+        std::cout << t.delivery << std::endl;
+    }
 }
 
 void callback_RD(const system_server::MsgRobotData &msg)
@@ -22,8 +35,9 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "echo");
     ros::NodeHandle nh;
-    ros::Subscriber subTask = nh.subscribe("server_request", 10, callback_RE);
-    ros::Subscriber subRobt = nh.subscribe("server_robot_data", 10, callback_RD);
+    ros::Subscriber subReq = nh.subscribe("/robot3/requests", 10, callback_RE);
+    ros::Subscriber subRob = nh.subscribe("server_robot_data", 10, callback_RD);
+    ros::Subscriber subTaskList = nh.subscribe("/robot3/task_list", 10, callback_TL);
     ros::spin();
 
     return 0;
